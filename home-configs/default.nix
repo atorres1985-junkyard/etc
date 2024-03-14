4af4df818af4ff3f71eb5d4b919611a6f9535a23
Anderson Torres <torres.anderson.85@protonmail.com>
2024-03-14 07:33:57 -0300

    [home-configs/anderson] esqueleto

--- home-configs/default.nix
+++ home-configs/default.nix
@@ -4,4 +4,15 @@
 }:
 
 {
+  anderson = home-manager.lib.homeManagerConfiguration {
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [
+      ];
+    };
+    modules = [
+      ./anderson/default.nix
+    ];
+    extraSpecialArgs.flake = flake;
+  };
 }
